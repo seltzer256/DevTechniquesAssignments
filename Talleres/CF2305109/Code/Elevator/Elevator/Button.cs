a914051a2633b82b564a6ee849815d68d8611777
Henry Steven Fuérez Morán <38507062+seltzer256@users.noreply.github.com>
2018-07-30 12:05:34 -0500

    Elevator System

---
+++ Talleres/CF2305109/Code/Elevator/Elevator/Button.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Button
+    {
+        private string[] type = {"O","C","U","D","A",""};
+        private bool state=false;
+
+        public bool State { get => state; set => state = value; }
+        public string[] Type { get => type; set => type = value; }
+
+        public void Press(Door Door, int Value)
+        {
+            if (state == false)
+            {
+                state = true;
+                switch (type[Value])
+                {
+                    case "O":
+                        Door.Open();
+                        break;
+                    case "C":
+                        Door.Close();
+                        break;
+                    default:
+                        Console.WriteLine("Operation Error...");
+                        break;
+                }
+            }
+            else
+            {
+                state = false;
+            }
+        }
+        public void ElevatorOptions(MotorSystem motor)
+        {
+            if (state == false)
+            {
+                state = true;
+                switch (type)
+                {
+                    case "U":
+
+                        break;
+                    case "D":
+
+                        break;
+                    case "A":
+
+                        break;
+                    default:
+                        Console.WriteLine("Operation Error...");
+                        break;
+                }
+            }
+            else
+            {
+                state = false;
+            }
+        }
+
+    }
+}
---
+++ Talleres/CF2305109/Code/Elevator/Elevator/MotorSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class MotorSystem
+    {
+        public void Up()
+        {
+
+        }
+        public void Down()
+        {
+
+        }
+        public void setHighSpeed()
+        {
+
+        }
+        public void setLowSpeed()
+        {
+
+        }
+    }
+}
